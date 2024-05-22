98e489c56f256e0bd464791711479a8e6ec69180
FahimNuman87 <fahiomnuman87@gmail.com>
2024-05-22 19:03:12 +0600

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,162 @@
-import Image from "next/image";
+'use client'
+import React, { useState, useEffect } from 'react';
+
+
 
 export default function Home() {
+  const [initialInvestment, setInitialInvestment] = useState(1000);
+  const [period, setPeriod] = useState(1);
+  const [rentalYield, setRentalYield] = useState(11);
+  const [capitalAppreciation, setCapitalAppreciation] = useState(2);
+  const [totalAssets, setTotalAssets] = useState(2);
+  const [totalTokens, setTotalTokens] = useState(0);
+  const [properties, setProperties] = useState([]);
+  const [selectedProperty, setSelectedProperty] = useState(null);
+
+  const [selectedOption, setSelectedOption] = useState('One Month');
+
+  const handleToggle = (option) => {
+    setSelectedOption(option);
+  };
+
+  const calculateReturn = () => {
+    const selectedPropertyRentalYield = selectedProperty ? selectedProperty.rentalYield : 0;
+    const rentalIncome = initialInvestment * (selectedPropertyRentalYield / 10) * period;
+    const capitalAppreciationAmount = initialInvestment * (capitalAppreciation / 100);
+    const totalAmount = initialInvestment + rentalIncome + capitalAppreciationAmount;
+    const tokens = totalAmount / 1000;
+    setTotalAssets(totalAmount);
+    setTotalTokens(tokens);
+  };
+
+  useEffect(() => {
+   
+    fetch('/fakedata/fake.json')
+      .then(response => response.json())
+      .then(data => setProperties(data))
+      .catch(error => console.error('Error fetching properties:', error));
+  }, []);
+
+  const handlePropertyChange = (property) => {
+    setSelectedProperty(property);
+    setRentalYield(property ? property.rentalYield : 0);
+  };
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className='item-center bg-gray-100'>Return Calculator</div>
+      <section className="py-12 flex flex-wrap justify-center bg-teal-100">
+        <div className="w-4/12 sm:mr-6">
+          <div className="bg-white rounded-t-2xl rounded-b-lg shadow-md p-8 sm:p-12 bg-green-50">
+            <h2 className="text-sm sm:text-2xl font-bold mb-4 text-teal-700">Select Property</h2>
+            <select
+              id="property"
+              className="border border-gray-300 rounded p-2 w-full mb-6"
+              onChange={(e) => handlePropertyChange(properties.find(property => property.name === e.target.value))}
+            >
+              {properties.map((property, index) => (
+                <option key={index} value={property.name}>{property.name}</option>
+              ))}
+            </select>
+            <div className="flex justify-between mt-4">
+        <button
+          className={`toggle-btn ${selectedOption === 'One Month' ? 'active' : ''}`} 
+          onClick={() => handleToggle('One Month')} 
+        >
+          One Month
+        </button>
+        <button
+          className={`toggle-btn ${selectedOption === 'One Day' ? 'active' : ''}`} 
+          onClick={() => handleToggle('One Day')} 
+        >
+          One Day
+        </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <div>
+              <label htmlFor="initialInvestment" className="block font-semibold mb-1 text-teal-700">Initial Purchase Amount (USD)</label>
+              <input
+                type="number"
+                id="initialInvestment"
+                className="border border-gray-300 rounded p-2 w-full"
+                value={initialInvestment}
+                onChange={(e) => setInitialInvestment(parseFloat(e.target.value))} />
+            </div>
+            <div className="mt-4">
+              <label htmlFor="period" className="block font-semibold mb-1 text-teal-700">Period (Years)</label>
+              <div className="flex items-center justify-between">
+                <input
+                  type="range"
+                  min={0}
+                  max={10}
+                  value={period}
+                  className="range"
+                  step={5}
+                  onChange={(e) => setPeriod(parseFloat(e.target.value))}
+                />
+                {period}
+              </div>
+            </div>
+            <div className="mt-4">
+              <label htmlFor="rentalYield" className="block font-semibold mb-1 text-teal-700">Expected Rental Yield (%)</label>
+              <input
+                type="number"
+                id="rentalYield"
+                className="border border-gray-300 rounded p-2 w-full"
+                value={rentalYield}
+                onChange={(e) => setRentalYield(parseFloat(e.target.value))} />
+            </div>
+            <div className="form-control">
+            <label className="label cursor-pointer">
+            <input type="radio" name="radio-10" className="radio checked:bg-blue-500" checked />
+              <span className="label-text">Last Month</span> 
+              
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label cursor-pointer">
+            <input type="radio" name="radio-10" className="radio checked:bg-blue-500" checked />
+              <span className="label-text">Average</span> 
+              
+            </label>
+          </div>
+            <div className="mt-4">
+              <label htmlFor="capitalAppreciation" className="block font-semibold mb-1 text-teal-700">Expected Capital Appreciation (%)</label>
+              <input
+                type="number"
+                id="capitalAppreciation"
+                className="border border-gray-300 rounded p-2 w-full"
+                value={capitalAppreciation}
+                onChange={(e) => setCapitalAppreciation(parseFloat(e.target.value))} />
+            </div>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mt-6" onClick={calculateReturn}>Calculate</button>
+          </div>
+          
+        </div>
+        <div className="max-w-lg w-full sm:w-auto gap-y-8">
+          <div className="bg-white rounded-lg shadow-md p-8 sm:p-12 bg-green-50">
+            <h2 className="text-xl sm:text-2xl font-bold mb-6 text-teal-700 border rounded-lg bg-emerald-100 text-center">Projected Income</h2>
+           <br/> <span><span></span></span>
+           <br/>   <p className="text-green-700 text-2xl text-center">Purchase of USD{initialInvestment} = {totalTokens.toFixed(0)} Token</p><span><span></span></span>
+           <br/>   <p className="text-green-700 text-center ">Estimated rental yield to be received in {period} year(s): <span className='text-2xl'>USD{initialInvestment  * (rentalYield / 10) * period} </span> <br/> ({(initialInvestment * (rentalYield / 100) * period) / 1000} Token)</p><span><span></span></span>
+          
+          </div>
+          <br/>
+          <div className='bg-white rounded-lg shadow-md  sm:p-12 bg-white-50'>
+             <h2 className=" sm:text-sm font-bold  text-gray-700 ">Total assets in {period} year(s)</h2> 
+            <p className="text-2xl text-green-700 text-bold"><span className='text-2xl'>USD{totalAssets}</span> <br/> ({totalTokens.toFixed(0)} Token)</p>
+          </div>
+        </div>
+      </section>
+      <section className="py-12">
+        <div className="max-w-3xl mx-auto text-center">
+          <div className="bg-white rounded-lg shadow-md p-8 sm:p-12 mt-8">
+            <div className="text-sm">
+              <p className="text-gray-700 text-teal-700">Auto purchase with rental income</p>
+              <p className="italic text-gray-600">Disclaimer: The calculator is intended solely for illustrative purposes, and the generated information should not be construed as legal or financial advice, nor as a guarantee of any kind. The results produced by this calculator are estimates based on the input provided by the user and do not reflect actual results.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
---
+++ src/app/return/page.js
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react';
+
+const ReturnCalculator = () => {
+    return (
+        <div>
+            <div class="flex gap-4">
+    
+    <div class="flex-1 w-90 border rounded-lg">
+      02
+    </div>
+    <div class="flex-1 w-36 border rounded-lg">
+      03
+    </div>
+  </div>
+        </div>
+    );
+};
+
+export default ReturnCalculator;
